0f725b0f84fca039b32b9b1fcd5a8615051139c0
esraaelmoalem87 <esraaelmoalem851@gmail.com>
2026-08-06 11:46:46 +0300

    Fix merge: align App routes/imports to new pages (use pages Login/Register/Profile/Wishlist/NotFound)

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
-import ShopPage from "./pages/ShopPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import Login from "./components/Authentication/Login";
-import Signup from "./components/Authentication/Signup";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import { CartProvider } from "./context/CartContext";
-import CartPage from "./pages/CartPage";
-import "./App.css";
+import AboutPage from './pages/AboutPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
+import ContactPage from './pages/ContactPage';
+import ShopPage from './pages/ShopPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+import WishlistPage from './pages/WishlistPage';
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import Signup from './components/Authentication/Signup';
+import { CartProvider } from './context/CartContext';
+import './App.css';
 
 function App() {
   return (
-  <CartProvider>
-    <BrowserRouter>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content ">
-          <Routes >
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ShopPage />} />
-            <Route path="/products/:id" element={<ProductDetailsPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate to="/products" replace />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  </CartProvider>
+    <CartProvider>
+      <BrowserRouter>
+        <div className="app-container">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/:id" element={<BlogPostPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/products" element={<ShopPage />} />
+              <Route path="/products/:id" element={<ProductDetailsPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/wishlist" element={<WishlistPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
